fb239fd6697e7e68d31003e25c78858e1243f6af
voynow <voynow99@gmail.com>
2023-06-16 15:07:13 -0400

    Incrementing version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='git2vec',
-    version='0.1.4',
+    version='0.1.5',
     description='A useful module for handling Git data.',
     author='Jamie Voynow',
     author_email='voynow99@gmail.com',
